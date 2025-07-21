1bf7ed2ecdd3b3e1d5c2cfcea9aecd11d2f66a2b
kavana <kavananavada03@gmail.com>
2025-07-21 20:09:31 +0530

    added movies panel

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import MovieCard from './MovieCard'
 
 function Movies() {
   return (
     <div>
-        Movies
-      
+        <div className='text-center text-2xl text-bold text-gray-300 p-5 m-5'>
+            Trending Movies
+
+        </div>
+      <div className='flex flex-row flex-wrap justify-around'>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+      </div>
     </div>
   )
 }
