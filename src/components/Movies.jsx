62b26e4f06c96b2e36418b9fb6a309097a6f2e74
kavana <kavananavada03@gmail.com>
2025-07-22 20:33:27 +0530

    used axios for data fetch from api

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect,useState } from 'react'
 import MovieCard from './MovieCard'
 import axios from 'axios'
+
 function Movies() {
   const [movies,setMovies]=useState([]);
+  const apiKey = import.meta.env.VITE_API;
 
   useEffect(()=>{
-    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=26d9c0030f0449cbf3a1339be6d73e97&language=en-US&page=1`).then(function(res){
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`).then(function(res){
         setMovies(res.data.results)
         console.log(res.data.results)
+        
       })
 },[])
   
