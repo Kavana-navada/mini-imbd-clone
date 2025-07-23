feed0013eab3fa98df8ebc23a4c304c47aea6b44
kavana <kavananavada03@gmail.com>
2025-07-23 11:57:05 +0530

    added search bar

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function WatchList() {
   return (
-    <div>Watchlist
-      
-    </div>
-  )
+    <>
+      <div className="flex justify-center my-4">
+        <input
+          type="text"
+          className="text-gray-300 border-gray-500 border-1 h-[2.5rem] w-[18rem] bg-gray-500/10 text-xl pl-2 outline-none" placeholder="Search Movies..."
+        />
+      </div>
+      <div></div>
+    </>
+  );
 }
 
-export default WatchList
+export default WatchList;
